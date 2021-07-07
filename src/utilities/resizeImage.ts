fbc05a253adc58a5e1519ba89c1b14957e1fbae9
Islam Sayed <islam.sayed8@gmail.com>
2021-07-07 12:10:24 +0200

    add comments & a display msgs for each error

--- src/utilities/resizeImage.ts
+++ src/utilities/resizeImage.ts
@@ -8,9 +8,12 @@ const resize = async (
   next: Function
 ) => {
   const query = req.query;
+  //convert width and height values to numbers
   const width = parseInt(query.width as unknown as string);
   const height = parseInt(query.height as unknown as string);
+
   const imageName = query.name as unknown;
+  //check if image previously resized and present in cash
   const cashedFiles = await fsPromises.readdir(`resized-images`);
   const file = cashedFiles.find(
     (file) => file === `${width}-${height}-${imageName}`
@@ -19,15 +22,18 @@ const resize = async (
     const image = await fsPromises.readFile(
       `resized-images/${width}-${height}-${imageName as string}`
     );
+
+    //display the resized image as a response
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(
-      '<p style="font-weight: bold; font-size: 36px;">Image Previously Resized </p><img src="data:image/jpeg;base64,'
+      '<p style="text-align: center; font-weight: bold; font-size: 36px;">Image Previously Resized </p><img style="margin-right: auto; margin-left: auto; display: block;" src="data:image/jpeg;base64,'
     );
     res.write(Buffer.from(image).toString("base64"));
     res.end('"/>');
     return file;
   } else {
     try {
+      //if not present in cash resize it.
       const outPutImage = await sharp(`images/${query.name}`)
         .resize(width, height, { fit: "contain" })
         .toFile(`resized-images/${width}-${height}-${imageName as string}`);
@@ -35,21 +41,28 @@ const resize = async (
       const image = await fsPromises.readFile(
         `resized-images/${width}-${height}-${imageName as string}`
       );
-      console.log(image);
 
+    //display the resized image as a response
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(
-        '<p style="font-weight: bold; font-size: 36px;">Image Resized </p><img src="data:image/jpeg;base64,'
+        '<p style="text-align: center; font-weight: bold; font-size: 36px;">Image Resized </p><img style="margin-right: auto; margin-left: auto;  display: block;" src="data:image/jpeg;base64,'
       );
       res.write(Buffer.from(image).toString("base64"));
       res.end('"/>');
 
       return outPutImage;
+
     } catch (err) {
-      res.send(
-        `<p style="text-align: center; font-weight: bold; font-size: 36px;">${err.message}</p>`
-      );
-      console.info(err.message);
+      if (!imageName) {
+        res.send(
+          `<p style="text-align: center; font-weight: bold; font-size: 36px;">Please, write the image name!</p>`
+        );
+      }
+      if (!width || !height) {
+        res.send(
+          `<p style="text-align: center; font-weight: bold; font-size: 36px;">Be sure to add a width and height!</p>`
+        );
+      }
     }
   }
   next();
