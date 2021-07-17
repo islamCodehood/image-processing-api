2e800448aa7d385b46e4b619046111f2d4990f1e
Islam Sayed <islam.sayed8@gmail.com>
2021-07-17 10:13:22 +0200

    change info msgs

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,8 +4,10 @@ import image from "./image/image";
 const app = express();
 const routes = express.Router();
 
-routes.get("/", function(req: express.Request, res: express.Response): void {
-  res.send("<h2>Routes</h2>");
+routes.get("/", function (req: express.Request, res: express.Response): void {
+  res.send(
+    "<p>Please, write url in this way to get desired results:<p><p>http://localhost:{port-number}/api/image?name={image-name.jpg}&width={desired-width}&height={desired-height}</p>"
+  );
 });
 
 routes.use("/image", image);
